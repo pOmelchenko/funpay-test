b0b30313599e4633c88f0978a0bb1b790657d9b8
Pavel Omelchenko <p.Omelchenko@gmail.com>
2024-04-04 19:09:18 +0300

    Перенес реализацию из index.php

--- Database.php
+++ Database.php
@@ -16,7 +16,49 @@ class Database implements DatabaseInterface
 
     public function buildQuery(string $query, array $args = []): string
     {
-        throw new Exception();
+        if ([] === $args) return $query;
+
+        $res = '';
+
+        for ($i = 0; $i < strlen($query); $i++) {
+            if ($query[$i] === '?') {
+                $res .= match ($query[$i + 1]) {
+                    'd' => (int) array_shift($args),
+                    'f' => (float) array_shift($args),
+                    'a' => $this->buildArray(array_shift($args)),
+                    '#' => "`" . implode("`, `", (array) array_shift($args)) . "`",
+                    ' ' => "'" . array_shift($args) . "'",
+                    default => $query[$i],
+                };
+                $query[$i + 1] !== ' ' && $i++;
+            } else {
+                $res .= $query[$i];
+            }
+
+        }
+
+        return $res;
+    }
+
+    private function buildArray($args): string
+    {
+        if (!array_is_list($args)) {
+            $args = array_map(
+                fn ($k, $v) => "`$k` = " . $this->getVal($v),
+                array_keys($args),
+                array_values($args)
+            );
+        }
+        return implode(", ", $args);
+    }
+
+    private function getVal($arg): string
+    {
+        return match (gettype($arg)) {
+            'string' => "'$arg'",
+            'NULL'=> 'NULL',
+            default => $arg,
+        };
     }
 
     public function skip()
