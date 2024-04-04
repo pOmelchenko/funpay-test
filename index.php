eeeb72e662a13e6e79fdae784900917a76e53a03
asaifutdinov <asaifutdinov@bssl.ru>
2024-04-04 17:16:04 +0300

    пятая попытка

--- index.php
+++ index.php
@@ -6,13 +6,13 @@ function buildQuery(string $query, array $args = []): string
 
     $res = '';
 
-    for ($i = 0; $i < mb_strlen($query); $i++) {
+    for ($i = 0; $i < strlen($query); $i++) {
         if ($query[$i] === '?') {
             $res .= match ($query[$i + 1]) {
                 'd' => (int) array_shift($args),
                 'f' => (float) array_shift($args),
-                'a' => '',
-                '#' => '`' . implode('`, `', (array) array_shift($args)) . '`',
+                'a' => buildArray(array_shift($args)),
+                '#' => "`" . implode("`, `", (array) array_shift($args)) . "`",
                 ' ' => "'" . array_shift($args) . "'",
                 default => $query[$i],
             };
@@ -26,8 +26,27 @@ function buildQuery(string $query, array $args = []): string
     return $res;
 }
 
+function buildArray($args) {
+    if (!array_is_list($args)) {
+        $result = [];
+        foreach ($args as $k => $v) {
+            $result[] = "`$k`=".getval($v);
+        }
+        return implode(",",$result);
+    }
+    return implode(",", $args);
+}
 
-echo buildQuery('SELECT name FROM users WHERE user_id = asd');
+function getval($arg):string {
+    return match (gettype($arg)) {
+        'string' => "'$arg'",
+        'NULL'=> 'NULL',
+        default => $arg,
+    };
+}
+
+
+echo buildQuery('SELECT name FROM users WHERE user_id = 1');
 
 echo PHP_EOL;
 
@@ -41,4 +60,11 @@ echo PHP_EOL;
 echo buildQuery(
     'SELECT ?# FROM users WHERE user_id = ?d AND block = ?d',
     [['name', 'email'], 2, true]
+);
+
+echo PHP_EOL;
+
+echo buildQuery(
+    'UPDATE users SET ?a WHERE user_id = -1',
+    [['name' => 'Jack', 'email' => null]]
 );
